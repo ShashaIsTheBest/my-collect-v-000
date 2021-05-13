f97945b934c94ac624907e483f43ffaaac5337fc
Shasha Yan <shashayan@gmail.com>
2021-05-13 21:35:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(students)
+  students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  my_collect(students) do |name|
+    name.split(" ").first
+  end
+  array
+end
 
-
+def my_collect(language)
+  i = 0
+  collection = ['ruby', 'javascript', 'python', 'objective-c']
+  while i < array.length
+    my_collect(language) << yield (|lang|lang.upcase)
+    i += 1
+  end
+  collection
+end
